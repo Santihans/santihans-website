84d7171b807697dd5bb6a58cfdbbdddc7a982dec
christopheschwyzer <kingchiller@gmail.com>
2017-08-13 10:27:16 +0200

    Replace portfolio page with store

---
+++ pages/_lang/store.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <div class="page-header">
+    <h1>{{ $t('pages.store') }}</h1>
+  </div>
+  <div class="page-content boundaries">
+    <div class="construction">
+      {{ $t('construction') }}
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  head() {
+    return {
+      title: this.$t('pages.store')
+    }
+  },
+  i18n: {
+    messages: {
+      en: {
+        construction: 'Coming Soon'
+      },
+      de: {
+        construction: 'Im Aufbau'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'store.scss';
+</style>
