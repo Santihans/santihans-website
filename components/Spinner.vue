89f9129cc7a972eeb96a7bb05d3ac208b0528303
christopheschwyzer <kingchiller@gmail.com>
2018-01-11 09:52:00 +0100

    Update vuetify, replace spinner, adjustments

---
+++ components/Spinner.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="spinner">
+    <div class="spinner-wheel"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.spinner {
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  top: 0;
+
+  .spinner-wheel {
+    width: 30px;
+    height: 30px;
+    animation: rotate 1000ms infinite;
+    border: 3px solid currentColor;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#F96673',
+    accent: '#ce93d8',
+    secondary: '#424242',
+    info: '#0D47A1',
+    warning: '#ffb300',
+    error: '#B71C1C',
+    success: '#49a577'
+  }
+})
