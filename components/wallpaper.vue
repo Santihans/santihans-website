cc88f65f237fd4706e39c0c826e571d9f1cb2a98
christopheschwyzer <kingchiller@gmail.com>
2018-03-20 13:12:09 +0100

    Add video to index page

--- components/wallpaper.vue
+++ components/wallpaper.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="wallpaper">
     <div class="parallax">
-      <div class="parallax-image"></div>
+      <div class="parallax-image">
+        <video src="/video/Denkmal.mp4" autoplay muted loop poster="/video/Denkmal.jpg"></video>
+      </div>
       <div class="parallax-content">
         <div class="wallpaper-content boundaries">
           <h1 v-if="$i18n.locale === 'en'">We are a
             <span>premium</span> communications agency for tomorrow’s digital
-            <span>experiences.</span></h1>
+            <span>experiences.</span>
+          </h1>
           <h1 v-if="$i18n.locale === 'de'">Wir sind eine
             <span>Premium</span>-Kommunikations&shy;agentur für die digitalen
             <span>Erlebnisse</span> von morgen.</h1>
