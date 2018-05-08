cf74e43b14f06b0bc8ceb4811ce6725189426eca
kingchiller <kingchiller@gmail.com>
2018-05-08 22:13:38 +0200

    Add separation between "selected" and "more" projects

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -10,12 +10,33 @@
       <section class="loading" v-if="loading">
         <spinner />
       </section>
-      <div v-else class="boundaries" v-for="(item) in sortedProjects(projects)" :key="item._meta.id">
-        <nuxt-link class="link-box" :to="{ path: localePath('/work/' + `${item.urlSlug}`)}" :alt="$t('buttons.more')">
-          <div class="work" :style="'background-image:url('+ (item.headerImage ? item.headerImage.url : '') +');'">
-            <div class="work-inner">
+      <div v-else class="boundaries">
+        <nuxt-link class="link-box selected" v-for="(item) in sortedProjects(projects, true)" :key="item._meta.id" :to="{ path: localePath('/work/' + `${item.urlSlug}`)}" :alt="$t('buttons.more')" :style="'background-image:url('+ (item.headerImage ? item.headerImage.url : '') +');'">
+          <div class="selected-inner">
+            <tags :tags="item.tags" />
+            <h3 class="ellipsis">{{ item.title }}</h3>
+            <div class="description">
+              <template v-if="$i18n.locale === 'en'">
+                {{ item.abstractEn }}
+              </template>
+              <template v-if="$i18n.locale === 'de'">
+                {{ item.abstractDe }}
+              </template>
+            </div>
+            <span v-ripple class="s-btn s-btn-pink">{{ $t('buttons.more') }} </span>
+          </div>
+        </nuxt-link>
+
+        <section class="company boundaries">
+          <h2>{{ $t('more.heading') }}</h2>
+          <p class="abstract">{{ $t('more.abstract') }}</p>
+        </section>
+
+        <div class="grid">
+          <nuxt-link class="link-box more" v-for="(item) in sortedProjects(projects)" :key="item._meta.id" :to="{ path: localePath('/work/' + `${item.urlSlug}`)}" :alt="$t('buttons.more')">
+            <div class="more-content">
               <tags :tags="item.tags" />
-              <h2>{{ item.title }}</h2>
+              <h3 class="ellipsis">{{ item.title }}</h3>
               <div class="description">
                 <template v-if="$i18n.locale === 'en'">
                   {{ item.abstractEn }}
@@ -24,10 +45,10 @@
                   {{ item.abstractDe }}
                 </template>
               </div>
-              <span v-ripple class="s-btn s-btn-pink">{{ $t('buttons.more') }} </span>
             </div>
-          </div>
-        </nuxt-link>
+            <div class="more-image" :style="'background-image:url('+ (item.headerImage ? item.headerImage.url : '') +');'"></div>
+          </nuxt-link>
+        </div>
       </div>
       <contact-us/>
 
@@ -74,22 +95,27 @@ export default {
     }
   },
   methods: {
-    sortedProjects: function (items) {
-      const sortable = Object.keys(items).map(i => items[i])
-      const published = sortable.filter(item => item._meta.published === true)
-      published.sort(function (a, b) {
+    sortedProjects: function (items, selected = null) {
+      let sortable = Object.keys(items).map(i => items[i])
+      sortable = sortable.filter(item => item._meta.published === true)
+      sortable = sortable.filter(item => item.selected === selected)
+      sortable.sort(function (a, b) {
         return b.date - a.date
       })
-      return published
+      return sortable
     }
   },
   i18n: {
     messages: {
       en: {
         work: {
-          heading: 'Recent Highlights',
+          heading: 'Selected Projects',
           abstract:
-            'Our cross-functional, agile team creates custom communications solutions for demanding clients in a variety of fields.'
+            'A selection of nice stuff we recently created. Our work combines great design, state of the art technology and the ambition to create next-level online experiences.'
+        },
+        more: {
+          heading: 'There is more',
+          abstract: 'Smaller projects - not less ❤.'
         },
         heading: {
           scope: 'Project Scope',
@@ -98,9 +124,13 @@ export default {
       },
       de: {
         work: {
-          heading: 'Aktuelle Highlights',
+          heading: 'Ausgewählte Projekte',
           abstract:
-            'Unser funktionsübergreifendes, agiles Team entwickelt massgeschneiderte Kommunikationslösungen für anspruchsvolle Kunden aus den unterschiedlichsten Bereichen.'
+            'Eine Selektion aus kürzlich entstandenen Arbeiten. Unser Schaffen verbindet grossartiges Design, modernste Technologie und den Ehrgeiz, Online-Erlebnisse der nächsten Generation zu schaffen.'
+        },
+        more: {
+          heading: 'Es gibt noch mehr',
+          abstract: 'Kleinere Projekte - nicht weniger ❤.'
         },
         heading: {
           scope: 'Projektumfang',
@@ -129,11 +159,22 @@ export default {
   }
 
   .link-box {
-    display: block;
+    color: inherit;
     text-decoration: none;
+    overflow: hidden;
+    transition: 200ms;
+
+    &:hover {
+      box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.1);
+      transform: translateY(-1px);
+    }
   }
 
-  .work {
+  .tags {
+    font-weight: bold;
+  }
+
+  .selected {
     align-items: flex-end;
     background-position: center center;
     background-size: cover;
@@ -142,10 +183,10 @@ export default {
     font-size: 1.1em;
     height: 60vmax;
     justify-content: center;
+    margin-bottom: 5vmin;
     padding-bottom: 5vmin;
     padding-top: 5vmin;
     position: relative;
-    margin-bottom: 5vmin;
 
     @media screen and (min-width: $breakpointSmall) {
       height: 60vmin;
@@ -184,7 +225,7 @@ export default {
       width: 100%;
     }
 
-    .work-inner {
+    .selected-inner {
       max-width: 100%;
       position: relative;
       width: 90%;
@@ -195,7 +236,11 @@ export default {
       }
     }
 
-    h2 {
+    .tags {
+      margin-bottom: 0.6em;
+    }
+
+    h3 {
       font-size: 2.2em;
       line-height: 1;
 
@@ -213,9 +258,40 @@ export default {
     }
   }
 
-  .tags {
-    font-weight: bold;
-    margin-bottom: 0.6em;
+  .grid {
+    display: grid;
+    grid-gap: 15px;
+    grid-template-columns: 1fr;
+
+    @media screen and (min-width: 800px) {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  .more {
+    background-color: white;
+    border: 1px solid rgb(233, 233, 233);
+    display: flex;
+    font-size: 0.9em;
+
+    .more-content {
+      flex-grow: 1;
+      padding: 15px 15px 0;
+      overflow: hidden;
+    }
+
+    .more-image {
+      flex-shrink: 0;
+      width: 130px;
+      height: 130px;
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+
+    .tags {
+      color: $colorSubtle;
+      font-size: 0.9em;
+    }
   }
 }
 </style>
